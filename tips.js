a57c002c9ec308eda6f712bca069636c49f9ea98
kohrts <hfenaux@gmail.com>
2023-01-23 20:50:00 +0000

    round 119 tips

--- tips.js
+++ tips.js
@@ -8,9 +8,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const GOV_WEIGHT_THRESHOLD=500
-const START_BLOCK=25130001
-const END_BLOCK=25530000  //roughly 14,400 blocks per day
-const label = "round_118"
+const START_BLOCK=25530001
+const END_BLOCK=26020000  //roughly 14,400 blocks per day
+const label = "round_119"
 
 let db, users, reddit
 
